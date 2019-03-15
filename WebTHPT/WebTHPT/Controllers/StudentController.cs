811ac30f2ccf4e6fd32b8b578bcfda043eeeb33f
Loc Nguyen <locnguyen230598@gmail.com>
2019-03-15 21:57:40 +0700

    thong tin giao vien

--- WebTHPT/WebTHPT/Controllers/StudentController.cs
+++ WebTHPT/WebTHPT/Controllers/StudentController.cs
@@ -17,23 +17,6 @@ namespace WebTHPT.Controllers
             return View();
         }
 
-        public ActionResult ThongTinHocSinh()
-        {
-            var hs = (from k in db.Khois
-                      join l in db.Lops on k.MaKhoi equals l.MaKhoi
-                      join h in db.HocSinhs on l.MaLop equals h.MaLop
-                      where h.MaHS == "HS190001"
-                      select new
-                      {
-                          MaHS = h.MaHS,
-                          HoTenHS = h.HoTenHS,
-                          TenLop = l.TenLop,
-                          TenKhoi = k.TenKhoi,
-                          NienKhoa = h.NienKhoa,
-                          NgaySinh = h.NgaySinh
-                      }).OrderBy(c => c.MaHS).ToList();
-            return View(hs.ToList());
-        }
 
         public ActionResult DiemHocSinh()
         {
@@ -45,6 +28,11 @@ namespace WebTHPT.Controllers
 
             return View();
         }
+        public ActionResult ThongTinHocSinh()
+        {
+            return View();
+        }
+    
         public ActionResult GopY()
         {
             return View();
